5dd25c4d61de14167bb83cdbfd251a6fa8efe431
aubairakifSys <aubair.akif@systems.com>
2023-09-07 16:08:52 +0500

    changes

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -9,6 +9,7 @@ import { toast } from "react-hot-toast";
 import { Trash } from "lucide-react";
 import { Billboard } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
+// import { useParams, useRouter } from "next/navigation";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
